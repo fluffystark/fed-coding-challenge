91425fa06f1bc7d4e7a2485c74be8f3012aa17bd
John Matt <joflo.matres@gmail.com>
2020-05-22 17:19:02 +0800

    Add footer

--- src/App.js
+++ src/App.js
@@ -37,6 +37,25 @@ function App() {
         <div className="row">
           { cards }
         </div>
+        <div className="row">
+          <div className="col">
+            <div className="adrenalin-footer pt-4">
+              <div className="row">
+                <div className="col d-flex justify-content-between">
+                  <Logo className="logo footer" />
+                  <div className="d-flex flex-row flex-wrap">
+                    <NavItem>Privacy</NavItem>
+                    <NavItem>Sitemap</NavItem>
+                    <NavItem>Facebook</NavItem>
+                    <NavItem>Linkedin</NavItem>
+                    <NavItem>Instagram</NavItem>
+                    <NavItem>Twitter</NavItem>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
